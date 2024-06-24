bf83e32bfdb38a9b00b6af1eeb2bdb52821c22e7
Noe <8528445+NoeRGuerra@users.noreply.github.com>
2024-06-23 21:03:15 -0400

    Add event listeners to nav buttons

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import { buildHome } from "./home"
+import { buildMenu } from "./menu";
 
 buildHome();
+
+let mainContainer = document.querySelector("#content");
+let homeButton = document.querySelector('button:first-child');
+let menuButton = document.querySelector('button:nth-child(2)');
+let aboutButton = document.querySelector('button:last-child');
+
+homeButton.addEventListener('click', () => {
+    mainContainer.replaceChildren();
+    buildHome();
+})
+
+menuButton.addEventListener('click', () => {
+    mainContainer.replaceChildren();
+    buildMenu();
+})
