65f4afd05f4409d7f541ad521fb2ad15d0c7d50a
Noe <8528445+NoeRGuerra@users.noreply.github.com>
2024-06-27 01:54:35 -0400

    Style about page

--- src/about.js
+++ src/about.js
@@ -5,6 +5,7 @@ export function buildAboutPage() {
     let aboutText = document.createElement('p');
     aboutHeader.textContent = "About Us";
     aboutText.innerHTML = "Welcome to the Restaurant, where culinary excellence meets warm hospitality. Located in the heart of Metropolis, we use the freshest, locally-sourced ingredients to craft a menu that celebrates rich flavors. <br/> Our talented chefs blend classic techniques with innovative flair, offering dishes that are both timeless and contemporary. Whether you're here for a casual meal, a special celebration, or a romantic dinner, our inviting atmosphere and attentive service will make your visit memorable. <br/> Join us at the Restaurant and enjoy a dining experience that delights your taste buds and warms your heart."
+    aboutContainer.classList.add('about');
     aboutContainer.appendChild(aboutHeader);
     aboutContainer.appendChild(aboutText);
     container.appendChild(aboutContainer);
